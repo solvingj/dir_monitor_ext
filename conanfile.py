e5c8554a480c306f21ac95d6bc4fa0867e804199
Jerry W <jerrywiltse@gmail.com>
2017-10-13 22:13:16 -0400

    prep for upload

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,11 @@ class DirMonitorCMakeConan(ConanFile):
     name = "dir_monitor_ext"
     version = "0.1.0"
     description = "Extensions to the dir_monitor library"
-    author = "Solvingj"
     generators = "cmake"
     url = "https://github.com/bincrafters/dir_monitor_ext"
     exports_sources = "include/*"
     requires = "dir_monitor/1.0.0@bincrafters/stable", \
         "Boost.Asio/1.65.1@bincrafters/stable", \
-        "Boost.Log/1.65.1@bincrafters/stable"
 
     def package(self):
         self.copy(pattern="*", dst="include", src="include")
---
+++ test/file_monitor_tests.cpp
@@ -0,0 +1,7 @@
+#include "dir_monitor_ext/dir_monitor_ext.hpp"
+
+//TODO: Write basic test
+int main()
+{
+  return 0;
+}
