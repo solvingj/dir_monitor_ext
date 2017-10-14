9d5453bab3cf5ce910b12f2908de32413ba7bc09
Jerry W <jerrywiltse@gmail.com>
2017-10-13 22:15:34 -0400

    prep for upload

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -9,6 +9,7 @@ namespace dme = dir_monitor_ext;
 
 int main() 
 {
+
   dme::file_change_action fca([](bs::error_code ec, ba::dir_monitor_event dme)
   {
     std::cout << dme.path.filename().string() << " was changed and matched condition" << std::endl;
