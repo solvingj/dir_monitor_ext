a5b064eb61ae9c64f036f6c85721b35d263ab3ae
Jerry W <jerrywiltse@gmail.com>
2017-10-19 03:34:54 -0400

    switch repo to solvingj

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,15 @@
 from conans import ConanFile
 
-class DirMonitorCMakeConan(ConanFile):
+class DirMonitorExtCMakeConan(ConanFile):
     name = "dir_monitor_ext"
     version = "0.1.0"
     description = "Extensions to the dir_monitor library"
     generators = "cmake"
     license = "www.boost.org/license_1_0.txt"
-    url = "https://github.com/bincrafters/dir_monitor_ext"
+    url = "https://github.com/solvingj/dir_monitor_ext"
     exports_sources = "include/*"
     requires = "dir_monitor/1.0.0@bincrafters/stable", \
-        "Boost.Asio/1.65.1@bincrafters/stable", \
+        "Boost.Asio/1.65.1@bincrafters/stable"
 
     def package(self):
         self.copy(pattern="*", dst="include", src="include")
